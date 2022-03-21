fbed53b771b404b1a3ca2aa9849f4de18c53c7be
Benjababe <bengohzy@gmail.com>
2022-03-22 01:06:13 +0800

    tkinter restructuring to follow convention

--- main.py
+++ main.py
@@ -6,93 +6,119 @@ from wide import widen
 from wide_options import WideOptions
 
 
-def show_ui():
-    window = tk.Tk()
-    window.title("WebM Widener")
-    window.iconbitmap(ICON_PATH)
-    window.geometry("450x350")
+class WebmWidener(tk.Tk):
+    def __init__(self):
+        super(WebmWidener, self).__init__()
 
-    # main frame to contain everything
-    bg = tk.PhotoImage(file=BG_PATH)
-    frame_main = tk.Label(window, image=bg)
-    frame_main.pack()
+        self.setup_window()
+        self.setup_variables()
 
-    # label for file path
-    frame_file = tk.LabelFrame(frame_main, text='File')
-    frame_file.grid(row=1)
+        self.draw_main_frame()
+        self.draw_file_frame()
+        self.draw_status_frame()
 
-    var_file = tk.StringVar(frame_file, value="-")
+        self.draw_input_frame()
+        self.draw_encoder_frame()
+        self.draw_bitrate_frmae()
 
-    lbl_file = tk.Label(frame_file, textvariable=var_file)
-    lbl_file.pack(padx=6)
+        self.draw_widen_btn()
+    # end__init__
 
-    # label for current widening status
-    frame_status = tk.LabelFrame(frame_main, text='Status')
-    frame_status.grid(row=2)
+    def setup_window(self):
+        self.title("WebM Widener")
+        self.geometry("450x350")
+        self.iconbitmap(ICON_PATH)
+    # end_setup_window
 
-    var_status = tk.StringVar(frame_status, value="-")
+    def setup_variables(self):
+        self.var_file = tk.StringVar(self, value="-")
+        self.var_status = tk.StringVar(self, value="-")
+        self.var_input = tk.IntVar(self, value=0)
+        self.var_encoder = tk.StringVar(self, value="libvpx")
+    # end_setup_variables
 
-    lbl_status = tk.Label(frame_status, textvariable=var_status)
-    lbl_status.pack(padx=6)
+    def draw_main_frame(self):
+        self.bg = tk.PhotoImage(file=BG_PATH)
+        self.frame_main = tk.Label(self, image=self.bg)
+        self.frame_main.pack()
+    # end_draw_main
 
-    # opens file picker
-    def pick_file():
+    def draw_file_frame(self):
+        frame_file = tk.LabelFrame(self.frame_main, text='File')
+        frame_file.grid(row=1)
+
+        self.lbl_file = tk.Label(frame_file, textvariable=self.var_file)
+        self.lbl_file.pack(padx=6)
+
+        btn_file = tk.Button(
+            frame_file, text="Select File", command=self.pick_file
+        )
+        btn_file.pack(padx=6, pady=6)
+    # end_draw_file_frame
+
+    def draw_status_frame(self):
+        frame_status = tk.LabelFrame(self.frame_main, text='Status')
+        frame_status.grid(row=2)
+
+        self.lbl_status = tk.Label(
+            frame_status, textvariable=self.var_status
+        )
+        self.lbl_status.pack(padx=6)
+    # end_draw_status_frame
+
+    def pick_file(self):
         file_dir = tkinter.filedialog.askopenfilename(
             initialdir="./", title="Select File", filetypes=(("WebM files", "*.webm"), ("Image files", "*.jpg"), ("All files", "*"))
         )
-        var_file.set(file_dir)
-        window.update()
+        self.var_file.set(file_dir)
+        self.update()
     # end_pick_file
 
-    # adds file picker button to file frame
-    btn_file = tk.Button(frame_file, text="Select File", command=pick_file)
-    btn_file.pack(padx=6, pady=6)
+    def draw_input_frame(self):
+        frame_input = tk.LabelFrame(self.frame_main, text='Input Type')
+        frame_input.grid(row=3)
 
-    # populate radio buttons for selecting input type
-    frame_input = tk.LabelFrame(frame_main, text='Input Type')
-    frame_input.grid(row=3)
+        tk.Radiobutton(frame_input, text='WebM',
+                       value=0, variable=self.var_input).pack()
+        tk.Radiobutton(frame_input, text='Image (only jpegs currently supported)',
+                       value=1, variable=self.var_input).pack(anchor=tk.W)
+    # end_draw_input_frame
 
-    var_input = tk.IntVar(frame_input, value=0)
+    def draw_encoder_frame(self):
+        frame_encoder = tk.LabelFrame(self.frame_main, text='Video Encoder')
+        frame_encoder.grid(row=4)
 
-    tk.Radiobutton(frame_input, text='WebM',
-                   value=0, variable=var_input).pack()
-    tk.Radiobutton(frame_input, text='Image (only jpegs currently supported)',
-                   value=1, variable=var_input).pack(anchor=tk.W)
+        tk.Radiobutton(frame_encoder, text='libvpx (VP8, for posting on imageboards)',
+                       value="libvpx", variable=self.var_encoder).pack()
+        tk.Radiobutton(frame_encoder, text='libvpx-vp9 (VP9)',
+                       value="libvpx-vp9", variable=self.var_encoder).pack(anchor=tk.W)
+    # end_draw_encoder_frame
 
-    # populate radio buttons for selecting encoder type
-    frame_encoder = tk.LabelFrame(frame_main, text='Video Encoder')
-    frame_encoder.grid(row=4)
+    def draw_bitrate_frmae(self):
+        frame_bitrate = tk.LabelFrame(
+            self.frame_main, text='Video Bitrate (bps)')
+        frame_bitrate.grid(row=5)
 
-    var_encoder = tk.StringVar(frame_encoder, value="libvpx")
+        self.txt_bitrate = tk.Entry(frame_bitrate)
+        self.txt_bitrate.insert(0, 50000)
+        self.txt_bitrate.pack(padx=6, pady=6)
+    # end_draw_bitrate_frame
 
-    tk.Radiobutton(frame_encoder, text='libvpx (VP8, for posting on imageboards)',
-                   value="libvpx", variable=var_encoder).pack()
-    tk.Radiobutton(frame_encoder, text='libvpx-vp9 (VP9)',
-                   value="libvpx-vp9", variable=var_encoder).pack(anchor=tk.W)
-
-    # draws textbox for bitrate entry
-    frame_bitrate = tk.LabelFrame(frame_main, text='Video Bitrate (bps)')
-    frame_bitrate.grid(row=5)
-
-    txt_bitrate = tk.Entry(frame_bitrate)
-    txt_bitrate.insert(0, 50000)
-    txt_bitrate.pack(padx=6, pady=6)
-
-    def widen_file():
+    def start_widen(self):
         wide_options = WideOptions(
-            var_file.get(), var_encoder.get(), var_input.get(), int(txt_bitrate.get())
+            self.var_file.get(), self.var_encoder.get(),
+            self.var_input.get(), int(self.txt_bitrate.get())
         )
+        widen(self, self.var_status, wide_options)
+    # end_start_widen
 
-        widen(window, var_status, wide_options)
-    # end_widen_file
-
-    # draws buttons for widening process
-    btn_widen = tk.Button(frame_main, text="Start Widening",
-                          command=widen_file)
-    btn_widen.grid(row=7, pady=6)
-
-    window.mainloop()
+    def draw_widen_btn(self):
+        btn_widen = tk.Button(self.frame_main, text="Start Widening",
+                              command=self.start_widen)
+        btn_widen.grid(row=7, pady=6)
+    # end_draw_widen_btn
 
 
 if __name__ == "__main__":
-    show_ui()
+    widener = WebmWidener()
+    widener.mainloop()
